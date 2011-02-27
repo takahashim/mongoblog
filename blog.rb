78f7c95bc245f47387381b82e14954ee297eeae8
takahashim <maki@rubycolor.org>
2011-02-27 18:28:16 +0900

    basic認証と更新機能を追加

--- blog.rb
+++ blog.rb
@@ -12,6 +12,22 @@ require 'models'
 set :app_file, __FILE__
 set :haml, :format => :html5, :escape_html => true
 
+
+helpers do
+  def protected!
+    unless authorized?
+      response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+      throw(:halt, [401, "Not authorized\n"])
+    end
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == ['admin', 'adminpass']
+  end
+end
+
+
 get '/' do
   @posts = Post.all(:order => 'created_at DESC')
 
@@ -19,13 +35,23 @@ get '/' do
 end
 
 get '/posts/new' do
+  protected!
   @post = Post.new
 
   @title = 'New Post'
   haml :new
 end
 
+get '/posts/edit/:id' do |id|
+  protected!
+  @post = Post.find(id)
+
+  @title = 'Edit Post'
+  haml :new
+end
+
 post '/posts' do
+  protected!
   @post = Post.new(params[:post])
 
   if @post.save
